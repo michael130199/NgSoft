36791c1d94bb3f53e805c5aab922763c8d19ec4c
michael130199 <michael130199@gmail.com>
2019-09-23 20:54:57 -0400

    23/09/19 Modifique tienda y coloque tslint

--- src/app/components/tienda/tienda.component.ts
+++ src/app/components/tienda/tienda.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-//import { trigger, state, style, transition, animate } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 declare var jQuery:any;
 declare var $:any;
